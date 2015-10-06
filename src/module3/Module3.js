171345d3afaafec022ca41e99f6343e452c88d1e
epplestun <ivan.rodriguez@buntplanet.com>
2015-10-06 08:27:22 +0200

    Seed

--- src/module3/Module3.js
+++ src/module3/Module3.js
@@ -4,13 +4,21 @@ import {
   Runnable
 } from 'dali';
 
+import {DataShow} from 'directives/DataShow';
+
 @RouterConfig({
   title: 'Module 3',
   path : '/m3'
 })
 @View({
-  template: '<h2>Module3</h2>'
+  template: '<h2>Custom directive</h2><p *show="show">Text</p><button _click="toggle()">Click me!</button>'
 })
 @Runnable
 export class Module3 {
+  @Bindable
+  show = true;
+
+  toggle() {
+    this.show = !this.show;
+  }
 }
