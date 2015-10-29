ac499bc7589ab34da41a86759c0d0ca38ad97db5
ivan <ivan.rodriguez@buntplanet.com>
2015-10-29 22:58:20 +0100

    i18n example

--- src/main.js
+++ src/main.js
@@ -5,13 +5,32 @@ import {
   View,
   Bindable,
   Runnable,
-  RouterContent
+  RouterContent,
+  decorate,
+  i18nConfig
 } from 'dali';
 
 import {MenuBar} from 'menu-bar/MenuBar';
 import {Module1} from 'module1/Module1';
 import {Module2} from 'module2/Module2';
 import {Module3} from 'module3/Module3';
+
+let config = {
+  locale: 'en-US',
+  timezone: 'Europe/Madrid',
+  currency: 'EUR',
+  //translations: 'locale_en_US.json'
+  translations: {
+    en_US : {
+      app : {
+        title: 'Title',
+        total: 'Total: {{total, plural, =0 { You have no new messages } =1 { You have one new message } other { You have # new messages }}}',
+        gender: '{{friendGender, gender, male { Invite him } female { Invite her } other { Invite them }}}'
+      }
+    }
+  }
+};
+i18nConfig.init(config);
 
 @Component({
   name: 'app'
@@ -24,6 +43,9 @@ import {Module3} from 'module3/Module3';
 class App {
   @Bindable
   appName = "My Fist DALI App!!";
+
+  @Bindable
+  date = new Date();
 }
 
 bootstrap(App);
